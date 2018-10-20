df80c975875d67de0188bcf5e202142bdecd681f
ctapang <ctapang@hotmail.com>
2018-10-20 04:25:06 +0000

    Fixed problems in deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -19,34 +19,15 @@ module.exports = function(deployer, network, accounts) {
     return new web3.BigNumber(web3.toWei(n, 'ether'));
   }
 
-  const time = require('../test/helpers/time');
   const authorizedShares = new BigNumber(ether(11000000000));
 
   var rock = accounts[0];
-  var evangelist = accounts[1];
-  var vendor = accounts[2];
-  var beneficiary = accounts[3];
 
   if (network == "ropsten") {
     rock = address("0x95BfD3a00587d4c9EB6846eA7748777e798e7410");
-    evangelist = address("0x92d2c6b5516fae8d3c69a110cdd602efd48e27fe");
-    vendor = address("0x793d575b31922c6fefa4e63de12c3d4d7ae58398");
-    beneficiary = address("0xbcfdbe36869037be252bf2dfcfcd93b534fdd697");
   }
 
-  // +1 minute so it starts after contract instantiation
-  var start;
-  var cliffDuration;
-  var duration;
-  time.latest().then((latest) => { 
-    start = latest + time.duration.minutes(1);
-    cliffDuration = time.duration.months(6);
-    duration = time.duration.years(2) + cliffDuration;
-    return;
-  })
-  .then(() => {
-    return deployer.deploy(UniversalToken, ether(100000), 100, 3000);
-  })
+  deployer.deploy(UniversalToken, ether(100000), 100, 3000)
   .then((token) => {
     uToken = token;
     return deployer.deploy( LocalToken, ether(10000000), 0, "RST000001", "Local Token for Bellevue, WA (by RockStable Inc)", "Bellevue WA King County United States", 0, rock, uToken.address );
@@ -87,18 +68,12 @@ module.exports = function(deployer, network, accounts) {
   .then((token) => {
     return deployer.deploy( LocalToken, ether(10000000), 0, "RST000013", "Local Token for Indonesia (by RockStable Inc)", "Indonesia", 0, rock, uToken.address );
   })
-//  .then((lToken) => {
-//    return deployer.deploy(Payment, 0, evangelist, lToken.address, vendor, rock);
-//  })
   .then((pmnt) => {
     return deployer.deploy( PureMoney, ether(10000000000) );
   })
   .then((pMoney) => {
     return deployer.deploy(RSTIShares, accounts[0], authorizedShares);
   })
-//  .then((rShares) => {
-//    return deployer.deploy(TokenVesting, beneficiary, start, cliffDuration, duration, true, { from: rock });
-//  })
   .catch((err) => {
     console.log(err);
   });
