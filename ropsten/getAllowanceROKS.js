00eb08f1a50e7f0891387be6fd9092a9897870f5
Carlos C. Tapang <ctapang@hotmail.com>
2020-09-30 04:58:08 -0700

    BSCTestNetDeployment?.txt turns out to be OK

--- ropsten/getAllowanceROKS.js
+++ ropsten/getAllowanceROKS.js
@@ -1,10 +1,13 @@
 const PureMoneyContract = artifacts.require('PureMoney2');
 // const { ZERO_ADDRESS } = require('../helpers/constants');
 
-const owner = '0x640C46042b4C50b4f4910b044898e80701203c58'.toLowerCase();
+
+const owner = '0x3b20FE9D78e5Ce1035FCBbAf8C73779E1BDabc72'.toLowerCase(); // on BSC
+// const owner = '0x640C46042b4C50b4f4910b044898e80701203c58'.toLowerCase(); // on Ethereum
 // const pmtAccount = '0x1Fb18FE4a3b773d61E9851f54d35948114e4806E'.toLowerCase();
 
-const puremoney = '0xa3c0a5899ee55ac29ee03f104cc9b85e32f4efe4'; // PureMoney2
+const puremoney = '0x78e897D906e83391Ff0E437735F43452E6259C13'; // on BSC
+// const puremoney = '0xa3c0a5899ee55ac29ee03f104cc9b85e32f4efe4'; // PureMoney2
 
 function ether (n) {
   return web3.utils.toWei(n.toString(), 'ether');
@@ -14,7 +17,10 @@ let ROKS = null;
 let cap = ether(0);
 let actualOwner = owner;
 
-const _allowanceROKS = function(source, spender, callback) {
+module.exports = function(deployer, network, accounts) {
+  console.log('network: ', network);
+  console.log('address0: ', accounts[0]);
+  
   PureMoneyContract.at(puremoney.toLowerCase())
   .then((result, error) => {
     if (result) {
@@ -111,6 +117,6 @@ const _allowanceROKS = function(source, spender, callback) {
   });
 }
 
-module.exports = function(callback) {
-  _allowanceROKS('0x1fb18fe4a3b773d61e9851f54d35948114e4806e', '0x1ef01b6dbe2eecf145b0fb62a74b17eb94838db2', callback);
-}
+// module.exports = function(callback) {
+//   _allowanceROKS('0x1fb18fe4a3b773d61e9851f54d35948114e4806e', '0x1ef01b6dbe2eecf145b0fb62a74b17eb94838db2', callback);
+// }
